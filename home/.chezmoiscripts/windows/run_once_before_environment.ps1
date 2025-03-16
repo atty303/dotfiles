03ae0eea4ebc7359ca7049b41558a9bc5c06e1fb
Koji AGAWA <atty303@gmail.com>
2025-03-16 17:41:07 +0900

    .

---
+++ home/.chezmoiscripts/windows/run_once_before_environment.ps1
@@ -0,0 +1,14 @@
+$paths = @(
+    "C:\dev\.local\bin"
+)
+
+$paths | ForEach-Object {
+    $pathToAdd = $_
+    # パスが既に含まれているかチェック
+    $currentPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+    if (-not ($currentPath -split ";" -contains $pathToAdd)) {
+        # パスが含まれていない場合は追加
+        $newPath = $currentPath + ";" + $pathToAdd
+        [System.Environment]::SetEnvironmentVariable("Path", $newPath, [System.EnvironmentVariableTarget]::User)
+    }
+}
