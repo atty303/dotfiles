42b4c707e5af22c31fe792d56fae29bcc0d762f7
Koji AGAWA <atty303@gmail.com>
2025-03-28 00:18:02 +0900

    feat: add Gitpod CLI installation and update settings.json for remote platform configurations

--- home/.chezmoiscripts/windows/run_onchange_before_install-other.ps1
+++ home/.chezmoiscripts/windows/run_onchange_before_install-other.ps1
@@ -7,3 +7,8 @@ $targetPath = Join-Path $targetDir "atuin.exe"
 if (-not (Test-Path $targetPath)) {
     Invoke-WebRequest -Uri "https://github.com/atty303/dotfiles/releases/download/atuin/atuin.exe" -OutFile $targetPath
 }
+
+$targetPath = Join-Path $targetDir "gitpod.exe"
+if (-not (Test-Path $targetPath)) {
+    Invoke-WebRequest -Uri "https://releases.gitpod.io/cli/stable/gitpod-windows-amd64.exe" -OutFile $targetPath
+}
