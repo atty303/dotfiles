ad21993d1e910328381f6c352d709cff4674c8e9
Koji AGAWA <atty303@gmail.com>
2025-08-19 22:06:38 +0900

    fix(wezterm): update window padding and background opacity settings

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -5,9 +5,18 @@ config.font = wezterm.font("UDEV Gothic NFLG")
 config.font_size = 12.0
 config.color_scheme = "Dracula"
 config.window_decorations = "RESIZE|INTEGRATED_BUTTONS"
+config.tab_max_width = 32
 
-config.window_background_opacity = 0
-config.win32_system_backdrop = "Acrylic"
+config.window_padding = {
+  left = '0.5cell',
+  right = '0.5cell',
+  top = '0.5cell',
+  bottom = '0.5cell',
+}
+
+config.window_background_opacity = 0.8
+config.text_background_opacity = 1
+config.win32_system_backdrop = "Disable"
 
 config.default_prog = { 'nu', '-l' }
 
